d47489079cc02e2152c45eb97b969c516d084d53
Neil Garcia <neilgrande2002@gmail.com>
2025-10-05 14:35:02 +0800

    Added footer

--- homepage.php
+++ homepage.php
@@ -75,6 +75,12 @@
       </div>
 
     </div>
+
   </div>
+
+  <footer class="bg-[#e8ebed] text-center font-semibold p-3">
+      <span>iManage &copy; 2025 | All rights reserved.</span>
+  </footer>
+
 </body>
 </html>
