2a737bf32833e868b6b98cb7fd8bb351c0b64d7b
Neil Garcia <neilgrande2002@gmail.com>
2025-10-12 15:22:40 +0800

    Set connection to MySQL database

---
+++ inc/dbh.php
@@ -0,0 +1,12 @@
+<?php
+
+$dsn = "mysql:host=localhost;dbname=php_dev";
+$dbusername = "neilg";
+$dbpassword = "neilg";
+
+try {
+  $pdo = new PDO($dsn, $dbusername, $dbpassword);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $err) {
+  echo "Connection failed: " . $err->getMessage();
+}
